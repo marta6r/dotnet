2b9179b00ad6433aa5e17cb63c3977409f1df271
marta6r <165066708+marta6r@users.noreply.github.com>
2025-04-23 10:42:51 +0300

    Add files

---
+++ Day9/Task1/Program.cs
@@ -0,0 +1,52 @@
+﻿struct WORKER
+{
+    public string name;
+    public string post;
+    public int year;
+
+    public override string ToString()
+    {
+        return $"Name: {name}, Post: {post}, Year: {year}";
+    }
+
+    public int Experience()
+    {
+        return 2025 - year;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+    WORKER[] workers = new WORKER[10];
+    for (int i = 0; i < workers.Length; i++)
+    {    
+            Console.WriteLine("Введите фамилию и инициалы: ");
+            workers[i].name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите должность: ");
+            workers[i].post = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите год поступления на работу: ");
+            workers[i].year = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Array.Sort(workers, (x, y) => string.Compare(x.name, y.name));
+
+    foreach (WORKER worker in workers)
+    {
+        Console.WriteLine(worker);
+    }
+
+    Console.WriteLine("Введите стаж: ");
+    int input_experience = Convert.ToInt32(Console.ReadLine());
+
+    foreach (WORKER worker in workers)
+    {
+        if (worker.Experience() >= input_experience)
+        {
+            Console.WriteLine(worker.name);
+        }
+    }
+    }
+}
---
+++ Day9/Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿enum Post {programer = 150, ingener = 150, doctor = 150};
+
+class Accauntant
+{
+    public bool AskForBonus(Post post, int hours)
+    {
+        int hours_for_month = (int)post;
+        if (hours > hours_for_month)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите должность: ");
+        string input_post = Console.ReadLine();
+        Console.WriteLine("Введите количество отработанных часов: ");
+        int input_hours = Convert.ToInt32(Console.ReadLine());
+
+        if (Enum.TryParse(typeof(Post), input_post, true, out Post post))
+        {
+            Accauntant accauntant = new Accauntant();
+            var hasBonus= accauntant.AskForBonus(post, input_hours);
+            if (hasBonus){
+                Console.WriteLine("Положена премия");
+            }
+            else{
+                Console.WriteLine("Не положена премия");
+            }
+        }
+        else{
+            Console.WriteLine("Данной должности нет");
+        }
+
+    }
+}
