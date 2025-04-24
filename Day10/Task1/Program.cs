a4d8180bcafd6f877d19b0e4d7d65428f604e211
marta6r <165066708+marta6r@users.noreply.github.com>
2025-04-24 10:54:46 +0300

    Add files

---
+++ Day10/Task1/Program.cs
@@ -0,0 +1,97 @@
+﻿interface Ix
+{
+    void IxF0(string w);
+    void IxF1();
+}
+
+interface Iy
+{
+    void F0(string w);
+    void F1();
+}
+
+interface Iz
+{
+    void F0(string w);
+    void F1();
+}
+
+class TestClass : Ix, Iy, Iz
+{
+    public string w;
+
+    // Ix методы
+    public void IxF0(string w)
+    {
+        this.w = w;
+        if (w.Length > 2) 
+            this.w = w.Substring(0, w.Length - 2);
+        Console.WriteLine($"Ix.F0: {this.w}");
+    }
+
+    public void IxF1()
+    {
+        if (w.Length > 2) 
+            w = w.Substring(0, w.Length - 2);
+        Console.WriteLine($"Ix.F1: {w}");
+    }
+
+    // Общая неявная реализация Iy и Iz
+    public void F0(string w)
+    {
+        this.w = w;
+        if (w.Length > 2) 
+            this.w = w.Substring(2);
+        Console.WriteLine($"Iy/Iz.F0 (неявно): {this.w}");
+    }
+
+    public void F1()
+    {
+        if (w.Length > 2) 
+            w = w.Substring(2);
+        Console.WriteLine($"Iy/Iz.F1 (неявно): {w}");
+    }
+
+    // Явная реализация Iz
+    void Iz.F0(string w)
+    {
+        this.w = w;
+        if (w.Length > 0)
+            this.w = "-" + (w.Length > 1 ? w.Substring(1) : "");
+        Console.WriteLine($"Iz.F0 (явно): {this.w}");
+    }
+
+    void Iz.F1()
+    {
+        if (w.Length > 0)
+            w = "-" + (w.Length > 1 ? w.Substring(1) : "");
+        Console.WriteLine($"Iz.F1 (явно): {w}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TestClass obj = new TestClass();
+        obj.w = "ПримерСтроки";
+
+        // Вызов методов Ix
+        obj.IxF0(obj.w);
+        obj.IxF1();
+
+        // Вызов методов Iy (неявно)
+        Iy iyRef = obj;
+        iyRef.F0(obj.w);
+        iyRef.F1();
+
+        // Вызов методов Iz через приведение (явно)
+        ((Iz)obj).F0(obj.w);
+        ((Iz)obj).F1();
+
+        // Вызов через интерфейсную ссылку
+        Iz izRef = obj;
+        izRef.F0(obj.w);
+        izRef.F1();
+    }
+}
---
+++ Day10/Task2/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+
+class Train : IComparable<Train>
+{
+    private string destination;
+    private string trainNumber;
+    private DateTime departureTime;
+
+    public Train(string destination, string trainNumber, DateTime departureTime)
+    {
+        this.destination = destination;
+        this.trainNumber = trainNumber;
+        this.departureTime = departureTime;
+    }
+
+    // Свойства
+    public string Destination => destination;
+    public string TrainNumber => trainNumber;
+    public DateTime DepartureTime => departureTime;
+
+    // Реализация IComparable
+    public int CompareTo(Train other) => departureTime.CompareTo(other.departureTime);
+
+    // Перегрузка операторов
+    public static bool operator >(Train t1, Train t2) => t1.departureTime > t2.departureTime;
+    public static bool operator <(Train t1, Train t2) => t1.departureTime < t2.departureTime;
+    public static bool operator >=(Train t1, Train t2) => t1.departureTime >= t2.departureTime;
+    public static bool operator <=(Train t1, Train t2) => t1.departureTime <= t2.departureTime;
+
+    public override string ToString() => 
+        $"Поезд #{trainNumber} до {destination}, отправление: {departureTime:HH:mm}";
+}
+
+class RailwayStation
+{
+    private Train[] trains;
+
+    public RailwayStation(Train[] trains)
+    {
+        this.trains = trains;
+        Array.Sort(this.trains); // Сортировка при инициализации
+    }
+
+    // Индексатор
+    public string this[string number]
+    {
+        get
+        {
+            var train = trains.FirstOrDefault(t => t.TrainNumber == number);
+            return train?.ToString() ?? "Поезд не найден";
+        }
+    }
+
+    // Поиск по времени
+    public void ShowAfterTime(DateTime time)
+    {
+        var result = trains.Where(t => t.DepartureTime.TimeOfDay > time.TimeOfDay)
+                          .OrderBy(t => t.DepartureTime);
+        
+        Console.WriteLine($"\nПоезда после {time:HH:mm}:");
+        foreach (var t in result) Console.WriteLine(t);
+    }
+
+    // Поиск по пункту назначения
+    public void ShowDestinationTrains(string destination)
+    {
+        var result = trains.Where(t => t.Destination.Equals(destination, StringComparison.OrdinalIgnoreCase))
+                          .OrderBy(t => t.DepartureTime);
+        
+        Console.WriteLine($"\nПоезда до {destination}:");
+        foreach (var t in result) Console.WriteLine(t);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем массив поездов
+        Train[] trains = {
+            new Train("Москва", "145A", new DateTime(2023, 1, 1, 14, 30, 0)),
+            new Train("Санкт-Петербург", "78Б", new DateTime(2023, 1, 1, 9, 15, 0)),
+            new Train("Москва", "КН2023", new DateTime(2023, 1, 1, 18, 45, 0)),
+            new Train("Новосибирск", "ЭКСПРЕСС", new DateTime(2023, 1, 1, 6, 0, 0))
+        };
+
+        RailwayStation station = new RailwayStation(trains);
+
+        // Демонстрация возможностей
+        Console.WriteLine("Поиск по номерам:");
+        Console.WriteLine(station["145A"]);
+        Console.WriteLine(station["НЕСУЩЕСТВУЕТ"]);
+
+        Console.WriteLine("\nСравнение поездов:");
+        Console.WriteLine($"Поезд 1 > Поезд 2: {trains[0] > trains[1]}");
+        Console.WriteLine($"Поезд 2 < Поезд 3: {trains[1] < trains[2]}");
+
+        station.ShowAfterTime(new DateTime(2023, 1, 1, 12, 0, 0));
+        station.ShowDestinationTrains("Москва");
+    }
+}
