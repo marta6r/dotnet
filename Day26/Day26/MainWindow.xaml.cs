4dbf621768213c29838d90e7568bbfdd826f408a
marta6r <165066708+marta6r@users.noreply.github.com>
2025-05-17 11:31:26 +0300

    Add files

---
+++ Day26/Day26/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Xml.Linq;
+
+namespace XMLParking
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadXml(object sender, RoutedEventArgs e)
+        {
+            XDocument xmlDoc = XDocument.Load("XMLFile1.xml");
+            CarList.Items.Clear();
+            foreach (var car in xmlDoc.Descendants("Автомобиль"))
+            {
+                CarList.Items.Add($"Марка: {car.Element("Марка")?.Value}, Год выпуска: {car.Element("ГодВыпуска")?.Value}, Срок аренды: {car.Element("СрокАренды")?.Value} дней");
+            }
+        }
+    }
+}
